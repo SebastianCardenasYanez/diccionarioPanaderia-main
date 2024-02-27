da6a65663157819b40116a1eff472391db2a2b15
SebastianCardenasYanez <160754840+SebastianCardenasYanez@users.noreply.github.com>
2024-02-27 16:53:39 -0500

    intento panaderia

---
+++ panaderia.py
@@ -0,0 +1,61 @@
+menuPanes = dict({
+    "Pan": {
+        "Producto" :list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de ajo", "valor": 2500},
+            {"nombre": "Pan integral", "valor": 2500},
+            {"nombre": "Bagette", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ]),
+        "Promociones": list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ])
+    },
+    "Pan Salados": {
+        "Producto" :list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ]),
+        "Promociones": list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ])
+    },
+    "Pan Salados": {
+        "Producto" :list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ]),
+        "Promociones": list([
+            {"nombre": "Pan de trigo", "valor": 2500},
+            {"nombre": "Pan de trigo", "valor": 2500},
+        ])
+    }
+})
+
+
+
+
+    
